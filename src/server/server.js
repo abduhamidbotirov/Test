1d8b4a22de4fa311572fab9afd04e3406126c2c6
abduhamidbotirov <abduhamidbotirovwork@gmail.com>
2024-02-18 20:18:20 +0500

    s

---
+++ src/server/server.js
@@ -0,0 +1,17 @@
+import Telegram from "node-telegram-bot-api";
+const bot = new Telegram("6549933624:AAFuObit_LichTWxGpzoareH-TR0CIaUC0U", {
+    polling: true,
+});
+const chanalUniversal = "@jnsckajnask";
+bot.on("message", (msg) => {
+    if (msg.photo) {
+        const photoId = msg.photo[0].file_id;
+        bot.sendPhoto(chanalUniversal, photoId);
+    } else if (msg.document) {
+        const docId = msg.document.file_id;
+        bot.sendDocument(chanalUniversal, docId);
+    } else { 
+        const messageText = msg.text;
+        bot.sendMessage(chanalUniversal, messageText);
+    }
+}); 
